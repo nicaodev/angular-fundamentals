74a120c955a4b60d75efb8093e6ae011de121cf3
Nicaodev <alexandre.nicos@gmail.com>
2020-08-31 20:26:03 -0300

    topo.component criado.

--- fakeLingo/src/app/app.module.ts
+++ fakeLingo/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
