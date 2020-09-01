e5ef0b15ce492f284f3ca1d88efd8d86a8831359
Nicaodev <alexandre.nicos@gmail.com>
2020-09-01 20:53:33 -0300

    frases e $event

---
+++ fakeLingo/src/app/painel/frases.mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model';
+
+export const FRASES: Frase[] = [
+  { fraseEng: 'I like to learn', frasePtbr: 'Eu gosto de aprender' },
+  { fraseEng: 'I watch tv', frasePtbr: 'Eu assito tv' },
+  { fraseEng: 'How are you?', frasePtbr: 'Como vai você' },
+  { fraseEng: 'I like', frasePtbr: 'Eu gosto' }
+];
---
+++ fakeLingo/src/app/shared/frase.model.ts
@@ -0,0 +1,13 @@
+export class Frase {
+
+  // public fraseEng: string;
+  // public frasePtbr: string;
+  //ou
+
+  constructor(public fraseEng: string, public frasePtbr: string) {
+    // this.fraseEng = fraseEng;
+    // this.frasePtbr = frasePtbr;
+    //ou
+  }
+}
+
