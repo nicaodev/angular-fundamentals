dafdc6d879605140303be23e44b1156068661cc2
Nicaodev <alexandre.nicos@gmail.com>
2020-09-02 22:57:57 -0300

    exibindo [img] c ngFor

--- fakeLingo/src/app/tentativas/tentativas.component.ts
+++ fakeLingo/src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Coracao } from '../shared/coracao.model';
 
 @Component({
   selector: 'app-tentativas',
@@ -9,6 +10,11 @@ export class TentativasComponent implements OnInit {
 
   s2Vazio = '/assets/coracao_vazio.png';
   s2Cheio = '/assets/coracao_cheio.png';
+
+  coracoes: Coracao[] = [
+    new Coracao(true), new Coracao(true), new Coracao(true)
+    // nao é necessário os outros paramêtros pois na classe estão default.
+  ];
 
   ngOnInit() {
   }
