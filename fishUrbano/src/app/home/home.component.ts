92455bff20c138be4350539c018ba07ff752ad0d
Nicaodev <alexandre.nicos@gmail.com>
2020-09-05 21:22:22 -0300

    service no escopo do component, mock e model

--- fishUrbano/src/app/home/home.component.ts
+++ fishUrbano/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../services/ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService] // injetando serviços no escopo componente
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }
+
+  public ofertas: Oferta[];
 
   ngOnInit() {
+    this.ofertas = this.ofertasService.getOfertas();
+    console.log('tem oferta', this.ofertas);
   }
 
 }
