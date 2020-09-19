4bd6673cda14d8b4ccd3b34bc0c72a0ddd8d56b3
Nicaodev <alexandre.nicos@gmail.com>
2020-09-19 00:12:08 -0300

    Com property bind para salvar

--- fishUrbano/src/app/ordemCompra/ordemCompra.component.ts
+++ fishUrbano/src/app/ordemCompra/ordemCompra.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  endereco: string = '';
+  numero: number = 23;
+  complemento: string = '';
+  formaPagamento: string = '';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  atualizaEndereco(input: string): void {
+    this.endereco = input;
+  }
+  atualizaNum(input: number): void {
+    this.numero = input;
+  }
+  atualizaComplemento(input: string): void {
+    this.complemento = input;
+  }
+
+  atualizaFormaPag(input: string): void {
+    this.formaPagamento = input;
   }
 
 }
