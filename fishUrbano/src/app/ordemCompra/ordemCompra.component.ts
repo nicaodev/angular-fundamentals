c529d4e46235d02f20049388ae932772667fa827
Nicaodev <alexandre.nicos@gmail.com>
2020-09-20 17:43:01 -0300

    validacao dos campos e model pedido

--- fishUrbano/src/app/ordemCompra/ordemCompra.component.ts
+++ fishUrbano/src/app/ordemCompra/ordemCompra.component.ts
@@ -1,33 +1,95 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdemCompraService } from '../services/ordem-compra.service';
+
+
 
 @Component({
   selector: 'app-ordemCompra',
   templateUrl: './ordemCompra.component.html',
-  styleUrls: ['./ordemCompra.component.css']
+  styleUrls: ['./ordemCompra.component.css'],
+  providers: [OrdemCompraService]
 })
 export class OrdemCompraComponent implements OnInit {
 
   endereco: string = '';
-  numero: number = 23;
+  numero: string = '';
   complemento: string = '';
   formaPagamento: string = '';
-  constructor() { }
+  // Controle de validação dos campos
+  enderecoValido: boolean;
+  numeroValido: boolean;
+  complementoValido: boolean;
+  formaPagamentoValido: boolean;
+
+  // estados primitivos dos campos (pristine)
+
+  enderecoEstadoPrimitivo = true;
+  numeroEstadoPrimitivo = true;
+  complementoEstadoPrimitivo = true;
+  formaPagamentoEstadoPrimitivo = true;
+
+  formValido = 'disabled';
+
+  constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
+    // this.ordemCompraService.efetivarCompra();
   }
 
   atualizaEndereco(input: string): void {
     this.endereco = input;
+
+    this.enderecoEstadoPrimitivo = false;
+
+    if (this.endereco.length > 3) {
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
+
+    this.habilitaForm();
   }
-  atualizaNum(input: number): void {
+  atualizaNum(input: string): void {
     this.numero = input;
+    this.numeroEstadoPrimitivo = false;
+
+    if (this.numero.length > 0) {
+      this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
+    }
+    this.habilitaForm();
+
   }
   atualizaComplemento(input: string): void {
     this.complemento = input;
+    this.complementoEstadoPrimitivo = false;
+
+    if (this.complemento.length > 0) {
+      this.complementoValido = true;
+    }
+    this.habilitaForm();
   }
 
   atualizaFormaPag(input: string): void {
     this.formaPagamento = input;
+    this.formaPagamentoEstadoPrimitivo = false;
+
+    if (this.formaPagamento.length > 0) {
+      this.formaPagamentoValido = true;
+    } else {
+      this.formaPagamentoValido = false;
+    }
+    this.habilitaForm();
+  }
+
+  habilitaForm(): void {
+    if (this.enderecoValido === true && this.numeroValido === true && this.formaPagamentoValido === true) {
+      this.formValido = '';
+    } else {
+      this.formValido = 'disabled';
+    }
+
   }
 
 }
---
+++ fishUrbano/src/app/services/ordem-compra.service.ts
@@ -0,0 +1,10 @@
+import { Pedido } from '../shared/pedido.model';
+
+
+
+export class OrdemCompraService {
+
+  public efetivarCompra(pedido: Pedido): void {
+    alert('Chegamos até aqui');
+  }
+}
