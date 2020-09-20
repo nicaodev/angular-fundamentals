173cec96b9400059cfaff1565ee3e63e5242591d
Nicaodev <alexandre.nicos@gmail.com>
2020-09-20 18:23:25 -0300

    s

--- fishUrbano/src/app/services/ordem-compra.service.ts
+++ fishUrbano/src/app/services/ordem-compra.service.ts
@@ -1,10 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
+import { URL_API } from 'src/environments/app.api';
 import { Pedido } from '../shared/pedido.model';
 
-
-
+@Injectable()
 export class OrdemCompraService {
+  constructor(private http: Http) { }
 
-  public efetivarCompra(pedido: Pedido): void {
-    console.log('O pedido é: ', pedido);
+  public efetivarCompra(pedido: Pedido): Observable<any> {
+    // URL, BOdY e Headers
+
+    let headers: Headers = new Headers();
+    return this.http.post(
+      `${URL_API}/pedidos`,
+      JSON.stringify(pedido),
+      new RequestOptions({headers: headers})
+    )
   }
 }
