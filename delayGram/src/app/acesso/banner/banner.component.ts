1d39851f2665e97aa5fa3714d1c5068073fe6c7b
Nicaodev <alexandre.nicos@gmail.com>
2020-09-26 20:05:24 -0300

    logica animacao img

--- delayGram/src/app/acesso/banner/banner.component.ts
+++ delayGram/src/app/acesso/banner/banner.component.ts
@@ -1,5 +1,6 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { Imagem } from './model/imagem.model';
 
 @Component({
   selector: 'app-banner',
@@ -19,11 +20,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
-  estadoAnimacaoInicial = 'escondido';
+  estadoAnimacaoInicial = 'visivel';
+  imagens: Imagem[] = [
+    { estado: 'visivel', url: '../../../assets/banner-acesso/img_1.png' },
+    { estado: 'escondido', url: '../../../assets/banner-acesso/img_2.png' },
+    { estado: 'escondido', url: '../../../assets/banner-acesso/img_3.png' },
+    { estado: 'escondido', url: '../../../assets/banner-acesso/img_4.png' },
+    { estado: 'escondido', url: '../../../assets/banner-acesso/img_5.png' }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => this.logicaRotacao(), 3000);
   }
+  public logicaRotacao(): void {
 
+    // Auxlia na exibição da imagem seguinte.
+    let aux: number;
+
+    for (let i = 0; i <= this.imagens.length; i++) {
+
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido';
+        aux = i === 4 ? 0 : i + 1; // loop no array.
+        break;
+      }
+    }
+    // Próxima img
+    this.imagens[aux].estado = 'visivel';
+
+    setTimeout(() => this.logicaRotacao(), 3000);
+  }
 }
