4c56304df6db4bc1e8bc06d14d40785b18d19fad
Nicaodev <alexandre.nicos@gmail.com>
2020-09-26 21:19:01 -0300

    output para login

--- delayGram/src/app/acesso/cadastro/cadastro.component.ts
+++ delayGram/src/app/acesso/cadastro/cadastro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,7 +9,13 @@ export class CadastroComponent implements OnInit {
 
   constructor() { }
 
+  @Output() mostraPainelLogin: EventEmitter<string> = new EventEmitter();
+
   ngOnInit() {
+  }
+
+  exibirPainelLogin() {
+    this.mostraPainelLogin.emit('login');
   }
 
 }
