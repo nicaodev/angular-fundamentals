e8974789e2121ebe669f3d071dca0f034a9446d2
Nicaodev <alexandre.nicos@gmail.com>
2020-09-27 17:28:43 -0300

    Sdk FIREBASE + post new user

--- delayGram/src/app/app.component.ts
+++ delayGram/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'delayGram';
+
+
+  ngOnInit(): void {
+    var firebaseConfig = {
+      apiKey: "AIzaSyAlRXDWMAppo_DFPGaNod_GLSF_Jj4ob4I",
+      authDomain: "delaygram-init.firebaseapp.com",
+      databaseURL: "https://delaygram-init.firebaseio.com",
+      projectId: "delaygram-init",
+      storageBucket: "delaygram-init.appspot.com",
+      messagingSenderId: "805584126973",
+      appId: "1:805584126973:web:db192ac8e9c422816a8f94",
+      measurementId: "G-SJ30QMXKHY"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
 }
--- delayGram/src/app/services/autenticacao.service.ts
+++ delayGram/src/app/services/autenticacao.service.ts
@@ -1,9 +1,23 @@
-import { Usuario } from "../models/usuario.model";
+import { Usuario } from '../models/usuario.model';
+
+import * as firebase from 'firebase';
 
 export class Autenticacao {
 
   public cadastrarUsuario(user: Usuario): void {
 
     console.log('chegamos no servico', user);
+
+    firebase.auth().createUserWithEmailAndPassword(user.email, user.senha)
+      .then((retorno: any) => {
+        // Removendo senha do obj a ser persistido.
+        delete user.senha;
+        // Registrando dados complementares do user no path(nó) com email na base 64(btoa).
+        firebase.database().ref(`usuario_detalhe/${btoa(user.email)}`)
+          .set(user);
+
+      }).catch((error: Error) => {
+        console.log('Erro', error);
+      });
   }
 }
