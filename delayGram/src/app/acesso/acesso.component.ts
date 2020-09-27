621aa31bb47a4520800c762c57f9bda7e16239cf
Nicaodev <alexandre.nicos@gmail.com>
2020-09-26 21:38:20 -0300

    keyframes

--- delayGram/src/app/acesso/acesso.component.ts
+++ delayGram/src/app/acesso/acesso.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -22,7 +22,15 @@ import { Component, OnInit } from '@angular/core';
       })),
       transition('void=> criado', [
         style({ opacity: 0, transform: 'translate(50px, 0)' }),
-        animate('500ms 500ms ease-in-out') // duracao, delay e/ou aceleracao.
+        animate('1000ms 250ms ease-in-out', keyframes([ // duracao, delay e/ou aceleracao.
+          style({ offset: 0.15, opacity: 1, transform: 'translateX(0)' }),
+          style({ offset: 0.86, opacity: 1, transform: 'translateX(0)' }),
+
+          style({ offset: 0.88, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.90, opacity: 1, transform: 'translateY(10px)' }),
+          style({ offset: 0.92, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.94, opacity: 1, transform: 'translateY(10px)' })
+        ]))
       ])
     ])
   ]
